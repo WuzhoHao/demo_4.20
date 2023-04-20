534052c7fad3f96e439e4880ba0613d87e0d9d01
fyg2022 <2795591565@qq.com>
2023-04-20 19:01:02 +0800

    123

---
+++ src/views/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">login</div>
+</template>
+
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+import {
+  ref,
+  reactive,
+  computed,
+  defineProps,
+  defineEmits,
+  defineExpose
+} from 'vue'
+
+// 引入路由
+const router = useRouter()
+// 路由对象
+const route = useRoute()
+// 引入仓库
+const store = useStore()
+
+// 声明响应式数据
+const data = reactive({})
+
+// 父传子
+const props = defineProps({})
+
+// 子传父
+const emit = defineEmits({})
+</script>
+
+<style lang="scss" scoped>
+.login {
+}
+</style>
